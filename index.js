2c05aec900f585e95a8ac67569ceb446abc2a842
Anoop Mundathan <anoop.mundathan@gmail.com>
2023-02-01 20:33:13 +0000

    Build

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello world - 1')
+    res.send('Hello world - 2 ')
 });
 
 app.listen(PORT, () => {
